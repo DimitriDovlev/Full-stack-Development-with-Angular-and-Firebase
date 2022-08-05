7d1caecedef3bc46f9c56456745577c814dbc80d
DimitriDovlev <dimitri.dovlev2000@gmail.com>
2022-08-05 15:07:33 +0200

    initial commit

---
+++ Class_01/index.ts
@@ -0,0 +1,12 @@
+// Declaring a variable means it exists
+// Initialization adds a value
+
+// Declaration
+let otherNumber: number;
+
+let myNumber = 5;
+const ojb = {
+  name: "Name",
+  age: 40,
+};
+
