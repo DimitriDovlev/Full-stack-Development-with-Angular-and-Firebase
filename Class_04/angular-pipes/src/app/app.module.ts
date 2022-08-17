d8da7f5acd5f351727b75523ec9acea5768dab14
DimitriDovlev <dimitri.dovlev2000@gmail.com>
2022-08-17 17:36:02 +0200

    Class_04 Code

--- Class_04/angular-pipes/src/app/app.module.ts
+++ Class_04/angular-pipes/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShortenPipe } from './pipes/shorten.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
+import { ExampleComponent } from './components/example/example.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe,
+    FilterPipe,
+    ExampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
